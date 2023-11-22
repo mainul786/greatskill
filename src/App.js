3e1e31d882f908a2af4239922d429c0ca4c35b26
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-11-22 22:52:51 +0530

    Registration design sucefully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Main from './layout/Main';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 const router = createBrowserRouter([
   {path:'/',
@@ -11,7 +13,15 @@ children:[
   {
     path:'/',
     element:<Home></Home>
-  }
+  },
+  {
+    path:'register',
+    element:<Register></Register>
+  },
+  {
+    path:'login',
+    element:<Login></Login>
+  },
 ]
 }
 ])
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
+
 const Register = () => {
     return (
-        <div>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold text-center">Register Here!</h1>
             
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name='name' placeholder="Type your name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name='email' placeholder="Type your email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name='password' placeholder="Type your password" className="input input-bordered" required />
+               
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     );
 };
 
